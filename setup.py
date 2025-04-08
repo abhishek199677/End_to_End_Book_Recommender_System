03d8eaa222ecd2009e4b4eb3527e65a5fb2d1d93
abhishek199677 <puttapoguabhishek1007@gmail.com>
2025-04-08 17:03:18 +0530

    setup file created

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+REPO_NAME = "End_to_End_Book_Recommender_System"
+AUTHOR_USER_NAME = "abhishek199677"
+SRC_REPO = "books_recommender"
+LIST_OF_REQUIREMENTS = []
+
+
+setup(
+    name=SRC_REPO,
+    version="0.0.1",
+    author="abhishek199677",
+    description="A small local packages for ML based books recommendations",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/abhishek199677/End_to_End_Book_Recommender_System",
+    author_email="puttapoguabhishek1007@gmail.com",
+    packages=find_packages(),
+    license="MIT",
+    python_requires=">=3.7",
+    install_requires=LIST_OF_REQUIREMENTS
+)
